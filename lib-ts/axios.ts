bc60aa8909fb51c097aa0aa3a626a135102e76e6
MissionLee <missionlee@163.com>
2020-03-28 17:02:31 +0800

    加入 自动缓存功能

--- lib-ts/axios.ts
+++ lib-ts/axios.ts
@@ -1,5 +1,5 @@
 // import PromiseCacheController = require('./helper/CacheController')
-import PromiseCacheController from "../node_modules/promise-cache-controller/index"
-PromiseCacheController.setCache("a","hello");
-console.log(PromiseCacheController.getCache("a"))
-console.log(PromiseCacheController.getCache("b"))
+// import PromiseCacheController from "../node_modules/promise-cache-controller/index"
+// PromiseCacheController.setCache("a","hello");
+// console.log(PromiseCacheController.getCache("a"))
+// console.log(PromiseCacheController.getCache("b"))
